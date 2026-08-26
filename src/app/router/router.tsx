19355075257be0f45747cf145c15cdaaf98f3fb4
framirezj <12649259+framirezj@users.noreply.github.com>
2026-08-26 12:12:57 -0400

    Configuracion error 404

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -5,38 +5,43 @@ import { AnexosPage } from "@/components/anexos/AnexosPage";
 import Layout from "../layouts/Layout";
 import { EnlacesPage } from "@/components/enlaces/EnlacesPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: (
+        <Layout>
+          <Outlet />
+        </Layout>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <InicioPage />,
+        },
+        {
+          path: "/bienestar",
+          element: <BienestarPage />,
+        },
+        {
+          path: "/dashboard",
+          element: <p>Dashboard</p>,
+        },
+        {
+          path: "/anexos",
+          element: <AnexosPage />,
+        },
+        {
+          path: "/personas",
+          element: <Navigate to="/anexos" replace />,
+        },
+        {
+          path: "/enlaces",
+          element: <EnlacesPage />,
+        },
+      ],
+    },
+  ],
   {
-    element: (
-      <Layout>
-        <Outlet />
-      </Layout>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <InicioPage />,
-      },
-      {
-        path: "/bienestar",
-        element: <BienestarPage />,
-      },
-      {
-        path: "/dashboard",
-        element: <p>Dashboard</p>,
-      },
-      {
-        path: "/anexos",
-        element: <AnexosPage />,
-      },
-      {
-        path: "/personas",
-        element: <Navigate to="/anexos" replace />,
-      },
-      {
-        path: "/enlaces",
-        element: <EnlacesPage />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
